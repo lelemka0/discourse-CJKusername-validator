8b7559f7b75f72af17c0d004f675409d7b769498
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2019-01-28 01:12:16 +0800

    Fix sanitize_username
    Fixed the exception of user name when the first word is CJK.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: CJKusername-validator
 # about:  CJK username support, supported SSO.
-# version: 0.2
+# version: 0.3
 # authors: lelemka0
 
 after_initialize do
@@ -13,7 +13,7 @@ after_initialize do
       # add CJK support
       name.gsub!(/[^\w\u4E00-\u9FD5\u3400-\u4DBF\u{20000}-\u{2A6DF}\u{2A700}-\u{2CEAF}\uF900–\uFAFF\u{2F800}-\u{2FA1D}\uAC00–\uD7AF\u3040-\u30FF\u31F0–\u31FF\u{1B000}–\u{1B0FF}\u3005.-]/, "_")
       # 2. removes unallowed leading characters
-      name.gsub!(/^\W+/, "")
+      name.gsub!(/^\W\u4E00-\u9FD5\u3400-\u4DBF\u{20000}-\u{2A6DF}\u{2A700}-\u{2CEAF}\uF900–\uFAFF\u{2F800}-\u{2FA1D}\uAC00–\uD7AF\u3040-\u30FF\u31F0–\u31FF\u{1B000}–\u{1B0FF}\u3005+/, "")
       # 3. removes unallowed trailing characters
       name = remove_unallowed_trailing_characters(name)
       # 4. unify special characters
